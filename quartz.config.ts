47e2aa76bc1f6f75468971e6f3a31d9cf23f2c20
ashna-khemani <ashna.khemani@gmail.com>
2026-07-26 21:48:38 -0400

    Quartz sync: Jul 26, 2026, 9:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "methinks.md",
+    pageTitle: "Ashna Khemani",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
